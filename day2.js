258738e78ed8a7638198b4e08e2784141b2d8121
Yansa27 <juliansa837@gmail.com>
2022-11-25 00:21:15 +0700

    push

---
+++ day2.js
@@ -0,0 +1,19 @@
+5 // merupakan sebuah expressiom
+2 + 3  // merupakan sebuah statement
+
+// variabel 
+let myname;
+
+myname = 10;
+
+console.log(myname) ;
+
+// cara simpel 
+let name = "juliansa";
+
+console.log(name);
+
+// variabel yang nilai nya tidak bisa di ubah 
+const fullname = "Juliansa"
+
+console.log(fullname)
