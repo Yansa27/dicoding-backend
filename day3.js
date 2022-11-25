ede20b098449063c0069485250453f8cc3f80fc6
Yansa27 <juliansa837@gmail.com>
2022-11-25 23:39:25 +0700

    push

---
+++ day3.js
@@ -0,0 +1,31 @@
+// type off cek jenis nilai variabel
+
+let x ;
+
+console.log(typeof(x)) ;
+
+
+// Aritmatika 
+
+let num1 = 10
+let num2 = 5
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+// operator (x++) incremet & (--) decrement 
+// Jika dituliskan setelah variabel (x++),
+// expression akan menghasilkan nilai variabel sebelum ditingkatkan nilainya
+let posfix = 5
+console.log(posfix++)
+
+console.log(posfix)
+
+// Jika dituliskan sebelum variabel (++x), 
+// expression akan menghasilkan nilai variabel setelah ditingkatkan nilainya.
+let prefix = 5 
+
+console.log(++prefix)
