25fc7e988048844ed6b09c68cca724e2085adb6e
Yansa27 <juliansa837@gmail.com>
2022-11-27 22:13:51 +0700

    push

---
+++ day4.js
@@ -0,0 +1,35 @@
+// bigint
+let number = 123456789123456156289189n
+let number1 = 12345678987678678786282
+
+console.log(number)
+console.log(number1)
+
+console.log(5n + 5n)
+console.log(4n - 6n)
+console.log(7n * 8n)
+console.log(8n / 8n)
+console.log(8n % 8n)
+
+// string 
+let nama = "juliansa"
+console.log(`nama saya ${nama}`)
+
+// boolean
+let a = 10
+let b = 12
+
+let isGreater = a > b
+let isLess = a < b
+
+console.log(isGreater)
+console.log(isLess)
+
+// null
+let somelaterdata = null
+console.log(somelaterdata)
+
+// symbol 
+let sym1 = Symbol("id") ;
+let sym2 = Symbol("id");
+console.log(sym1 == sym2)
