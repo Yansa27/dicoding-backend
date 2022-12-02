e4cb79199e59b9315855874f5a586dfa4deac0bf
Yansa27 <juliansa837@gmail.com>
2022-12-02 21:12:39 +0700

    push

---
+++ Dom/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").onclick = function () {
+    document.getElementById("text").innerHTML = "hallo deck"
+}
+document.getElementById("button1").onclick = function () {
+    document.getElementById("text2").innerHTML = "hello world!"
+}
+
