c5a17717963cfa4e87ff9c38f495c0d4c619d891
Yansa27 <juliansa837@gmail.com>
2022-12-02 23:11:56 +0700

    push

---
+++ day7.js
@@ -0,0 +1,38 @@
+// switch case 
+let name = "juliansa";
+let greeting = null;
+
+switch (name) {
+    case  "juliansa" :
+        greeting = "hello deck" ;
+        break;
+}
+
+console.log(greeting);
+
+for(let i = 0 ; i < 5 ; i++) {
+    console.log(i);
+}
+
+let Array = ["hallo","tes","tiga"]
+for(const myarray of Array) {
+    console.log(myarray);
+} 
+
+let i = 1;
+
+while(i <= 100) {
+    console.log(i)
+    i++;
+} 
+
+// do while
+
+let nilai = 1;
+
+do {
+    console.log(nilai);
+    nilai++;
+} while(nilai <= 100)
+
+
