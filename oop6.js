fff4d4cb95ca368134870dc4c1f0f2c59100a3da
Yansa27 <juliansa837@gmail.com>
2023-01-15 23:00:59 +0700

    push

--- oop6.js
+++ oop6.js
@@ -3,14 +3,31 @@ class MailService {
     constructor (sender) {
         this.sender = sender;
     }
+
+    sendMessage(message , receiver) {
+        console.log(`${this.sender} ${message} to ${receiver}`);
+    }
 }
 class WhatsAppService extends MailService {
 // constuctor overriding 
 constructor(sender , isBusiness){
+    // memanggil super class
     super(sender);
 
     this.isBusiness = isBusiness;
 }
+// overriding method 
+    sendMessage(message , receiver) {
+        // console.log(`${this.sender} ${message} to ${receiver} via whatsapp`);
+        super.sendMessage(message , receiver) ;
+
+        console.log('message sent via whatsaap')
+    }
 } 
 
+const mailservice = new MailService ('some sender');
+const whatsappService = new WhatsAppService ('085156390652' , true);
+
+mailservice.sendMessage('hai apa kabar' , 'some receiver');
+whatsappService.sendMessage('hai apa kabar?' , '085156390652');
 
