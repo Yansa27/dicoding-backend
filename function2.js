e4a2ec12b17f2c5d2a2eace076d21f1a0f6d0b3e
Yansa27 <juliansa837@gmail.com>
2023-01-24 20:41:47 +0700

    push

--- function2.js
+++ function2.js
@@ -1,2 +1,22 @@
 // imutability
 
+const user = {
+    firstname : "yansa",
+    lastname : "yansa27" // typo
+};
+
+const newNameWithLastname = (newlastname , user) => {
+    return{...user , lastname : newlastname};
+};
+
+const newUser = newNameWithLastname('yansa07' , user);
+
+console.log(newUser);
+
+// Rekursif 
+const countdown = start => {
+    console.log(start)
+    if(start > 0 ) countdown(start-1);
+}
+
+countdown(10);
