b6f362081d4668ae97a4d75e2a215be640c99f19
Yansa27 <juliansa837@gmail.com>
2023-02-05 22:11:02 +0700

    push

--- debug.js
+++ debug.js
@@ -1,9 +1,40 @@
 // penaganan eror 
-// Try and Catch
+// ! Try and Catch
+
+// try {
+//     console.log("awal blok try");
+//     errorCode;
+// } catch (eror) {
+//     console.log("terjadi eror");
+//     console.log(eror.name);
+//     console.log(eror.message);
+//     console.log(eror.stack);
+// ! name : Nama error yang terjadi.
+//! message : Pesan tentang detail error.
+//! stack : Informasi urutan kejadian yang menyebabkan error. Umumnya digunakan untuk debugging karena terdapat informasi baris mana yang menyebabkan error.
+// }
+
+// ! try-catch-finally
+// try {
+//     console.log("baris awal");
+//     errorCode;
+// } catch (error) {
+//     console.log("di abaikan");
+// }
+// finally {
+//     console.log("ok")
+// }
+
+
+// ! Throwing Errors
+let json = '{"name" : "Yoda", "age" : 20';
 
 try {
-    console.log("awal blok try");
-    errorCode;
+    let user = JSON.parse(json);
+
+    console.log(user.name);
+    console.log(user.age);
 } catch (error) {
-    console.log("terjadi eror");
+    console.log(error.name);
+    console.log(error.message);
 }
