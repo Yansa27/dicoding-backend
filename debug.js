8a4367dd687fcf79b37f738915ddd67db4cb7f92
Yansa27 <juliansa837@gmail.com>
2023-02-05 23:19:47 +0700

    push

--- debug.js
+++ debug.js
@@ -27,20 +27,31 @@
 
 
 // ! Throwing Errors
+class ValidationError extends error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError";
+    }
+}
 let json ='{"age": 20 }';
 
 try {
     let user = JSON.parse(json);
     if (!user.name){
-        throw new SyntaxError("'name' is not required");
+        throw new ValidationError("'name' is not required");
     }
+
+    if (!user.age) throw new ValidationError("'age' age is not required.");
     console.log(user.name);
     console.log(user.age);
 } catch (error) {
    if (error instanceof SyntaxError) {
     console.log(`JSON error : ${error.message}`);
-   } else if (error instanceof ReferenceError) {
-    console.log(eror.message);
+   } else if (error instanceof ValidationError)
+   {
+    console.log(`invalid data : ${error.message}`);
+   }else if (error instanceof ReferenceError) {
+    console.log(error.message);
    }else {
     console.log(error.stack);
    }
