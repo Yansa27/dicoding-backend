70b1d7695a28f20c97a77a7052b6a6bb4f4cc059
Yansa27 <juliansa837@gmail.com>
2023-02-11 00:06:08 +0700

    push

--- day10.js
+++ day10.js
@@ -7,3 +7,18 @@ setTimeout(()=> {
 }, 3000)
 
 console.log("ada yang bisa di bantu?");
+
+// call back function 
+const orderCoffe = () => {
+    let coffe = null
+    console.log("silahkan menunggu...");
+
+    setTimeout(() => {
+        coffe= "kopi sudah jadi";
+        callback(coffe)
+    }, 3000)
+}
+
+orderCoffe (coffe => {
+    console.log(coffe);
+})
