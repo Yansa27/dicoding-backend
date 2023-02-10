718b42a4334cacc82bbca0cc921592595d42429f
Yansa27 <juliansa837@gmail.com>
2023-02-10 23:56:59 +0700

    push

---
+++ day10.js
@@ -0,0 +1,9 @@
+// synchronous VS Asynchronous
+
+console.log("selamat datang");
+
+setTimeout(()=> {
+    console.log("selamat datang kembali");
+}, 3000)
+
+console.log("ada yang bisa di bantu?");
--- quizeror.js
+++ quizeror.js
@@ -61,4 +61,6 @@ const detectTriangle = (a, b, c) => {
   
     return 'Segitiga sembarang';
   };
+
+  
   
